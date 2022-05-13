31f4803b660a38093ace9d2456f8d6c4ea3585e3
xgiuseppevetrano <xgiuseppevetrano@gmail.com>
2022-05-13 21:53:08 +0200

    fix a bug

--- main.js
+++ main.js
@@ -1,47 +1,51 @@
-const teamMemberse = [
+// 1. Creo un array di oggetti
+const teamMembers = [
     {
         fullName: "Wayne Barnett",
         image: "img/wayne-barnett-founder-ceo.jpg",
-        job-position: "Founder & CEO",
+        jobPosition: "Founder & CEO",
     },
     {
         fullName: "Angela Caroll",
         image: "img/angela-caroll-chief-editor.jpg",
-        job-position: "Chief Editor",
+        jobPosition: "Chief Editor",
     },
     {
         fullName: "Walter Gordon",
         image: "img/walter-gordon-office-manager.jpg",
-        job-position: "Office Manager",
+        jobPosition: "Office Manager",
     },
     {
         fullName: "Angela Lopez",
         image: "img/angela-lopez-social-media-manager.jpg",
-        job-position: "Social Media Manager",
+        jobPosition: "Social Media Manager",
     },
     {
         fullName: "Scott Estrada",
         image: "img/scott-estrada-developer.jpg",
-        job-position: "Developer",
+        jobPosition: "Developer",
     },
     {
         fullName: "Barbara Ramos",
         image: "img/barbara-ramos-graphic-designer.jpg",
-        job-position: "Graphic Designer",
+        jobPosition: "Graphic Designer",
     },
 ];
 
+// 2. Prendo dall'HTML il container in cui inserirò le card
 const teamCardsContainer = document.querySelector(".team-container");
 
+// 3. Creo un ciclo per scorrere l'array di oggetti
 for (let i = 0; i < teamMembers.length; i++) {
+    // 3a. Stampo nell'HTML le card dei membri del team
     const card = `
         <div class="team-card">
             <div class="card-image">
-                <img src="${teamMembers[i].image}" alt="${teamMembers[i].name}">
+                <img src="${teamMembers[i].image}" alt="${teamMembers[i].fullName}">
             </div>
             <div class="card-text">
-                <h3>${teamMembers[i].name}</h3>
-                <p>${teamMembers[i].job-position}</p>
+                <h3>${teamMembers[i].fullName}</h3>
+                <p>${teamMembers[i].jobPosition}</p>
             </div>
         </div>
     `;
